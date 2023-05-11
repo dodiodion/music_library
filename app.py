2bb6dbf4ef414a8c0858018b717999bdcbb55ec1
dodiodion <dodiodion@gmail.com>
2023-05-11 16:06:42 +0200

    application code

--- app.py
+++ app.py
@@ -1,34 +1,38 @@
-from lib.database_connection import DatabaseConnection
-from lib.artist_repository import ArtistRepository
+# file: app.py
 from lib.album_repository import AlbumRepository
-from lib.recipe_repository import RecipeRepository
+from lib.artist_repository import ArtistRepository
+from lib.database_connection import DatabaseConnection
 
-# Connect to the database
-connection = DatabaseConnection()
-connection.connect()
+class Application():
+    def __init__(self):
+        self._connection = DatabaseConnection()
+        self._connection.connect()
+        self._connection.seed("seeds/music_library.sql")
 
-# Seed with some seed data
-connection.seed("seeds/music_library.sql")
+    def run(self):
+    # "Runs" the terminal application.
+    # It might:
+    #   * Ask the user to enter some input
+    #   * Make some decisions based on that input
+    #   * Query the database
+    #   * Display some output
+    # We're going to print out the artists!
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
+        artist_repository = ArtistRepository(self._connection)
+        artists = artist_repository.all()
+        album_repository = AlbumRepository(self._connection)
+        albums = album_repository.all()
+        print("Welcome to the music library manager!")
+        print("What would you like to do? \n 1 - List all albums \n 2 - List all artists\n")
+        choice = input("Enter your choice: ")
+        if(choice == "1"):
+            for album in albums:
+                print(f"{album.id} - {album.title})")
+        elif(choice == "2"):
+            for artist in artists:
+                print(f"{artist.id}: {artist.name} ({artist.genre})")
+        else: print (f"This is not a choice: {choice}")
 
-# List them out
-for artist in artists:
-    print(artist)
-
-#Retrieve all albums
-album_repository = AlbumRepository(connection)
-
-# List them out
-for album in album_repository.all():
-    print(album)
-
-print(album_repository.find(2))
-
-recipe_repository = RecipeRepository(connection)
-for recipe in recipe_repository.all():
-    print(recipe)
-
-print(recipe_repository.find(4))
+if __name__ == '__main__':
+    app = Application()
+    app.run()
